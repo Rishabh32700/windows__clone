413942ae96a714d851a13775ede001dfc7933a2b
Rishabh Sharma <sharmarishabh32700@gmail.com>
2022-05-22 13:13:21 +0530

    added typescript

---
+++ windows_clone/pages/index.tsx
@@ -0,0 +1,10 @@
+// export default function Home() {
+//   const message = 'hello world'
+//   const element = <h1>{message}</h1>
+//   return element
+// }
+export default function Home() {
+  return (
+    <h1>hello world !!!</h1>
+  )
+}
